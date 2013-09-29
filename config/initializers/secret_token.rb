d379d6ba09ad7301792b82e6431aa99d760225be
Leah <lifang.moler@gmail.com>
2013-09-29 16:00:35 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cmpe235project::Application.config.secret_key_base = '844fe85d70f68d4b4e585931ee48a8dff6804b78be038fe84d4a32c6234bf1e6d670fdee5a8d6515948da069de6886397ae03c3d54c036ad702728ecfb65fec9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cmpe235project::Application.config.session_store :cookie_store, key: '_cmpe235project_session'
