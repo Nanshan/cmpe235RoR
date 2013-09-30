7c8ff0f7d73c680274b7913b0c27e610e2eaae67
Leah <lifang.moler@gmail.com>
2013-09-29 18:47:42 -0700

    new event routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,38 @@
 class EventsController < ApplicationController
 skip_before_action :verify_authenticity_token
 
-  def new
-    @event = Event.new
-  end
-
   def show
     @event = Event.find(params[:id])
   end
 
-  def create
-    @event = Event.new(params[:event])
- 
+  def impression
+    @event = ImpressionEvent.new(params[:event])
     @event.save
-    redirect_to @event
+  end
+
+  def click
+    @event = ClickEvent.new(params[:event])
+    @event.save
+  end
+
+  def clickthru
+    @event = ClickThroughEvent.new(params[:event])
+    @event.save
+  end
+
+  def sms
+    @event = SMSEvent.new(params[:event])
+    @event.save
+  end
+
+  def map
+    @event = MapEvent.new(params[:event])
+    @event.save
+  end
+
+  def call
+    @event = CallEvent.new(params[:event])
+    @event.save
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Cmpe235project::Application.routes.draw do
 
-   resources :advertisements, :events
+   resources :advertisements
+   resources :events, only: [:index]
+   post '/events/impression', to: 'events#impression'
+   post '/events/click', to: 'events#click'
+   post '/events/clickthru', to: 'events#clickthru'
+   post '/events/sms', to: 'events#sms'
+   post '/events/map', to: 'events#map'
+   post '/events/call', to: 'events#call'
 
 end
