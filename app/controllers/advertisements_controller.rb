85f40ae2ffec02e114c8b61dba50f7863d33fcda
Leah <lifang.moler@gmail.com>
2013-09-29 18:27:56 -0700

    allow POSTs

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,4 +1,6 @@
 class AdvertisementsController < ApplicationController
+skip_before_action :verify_authenticity_token
+
   def new
     @advert = Advertisement.new
   end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+skip_before_action :verify_authenticity_token
+
   def new
     @event = Event.new
   end
