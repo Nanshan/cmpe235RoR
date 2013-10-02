e099fc0fe9ece08a2760df46f36cac37053df733
Leah <lifang.moler@gmail.com>
2013-10-01 20:26:46 -0700

    controller methods for all events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,28 +13,38 @@ skip_before_action :verify_authenticity_token
   end
 
   def click
-    @event = ClickEvent.new(params[:event])
-    @event.save
+    @advert = Advertisement.find_by name: params[:ad_name]
+    @event = ClickEvent.create(user_location: params[:user_location], user_phone_id: params[:user_phone_id])
+    @advert.events << @event
+    @advert.save
   end
 
   def clickthru
-    @event = ClickThroughEvent.new(params[:event])
-    @event.save
+    @advert = Advertisement.find_by name: params[:ad_name]
+    @event = ClickThroughEvent.create(user_location: params[:user_location], user_phone_id: params[:user_phone_id])
+    @advert.events << @event
+    @advert.save
   end
 
   def sms
-    @event = SMSEvent.new(params[:event])
-    @event.save
+    @advert = Advertisement.find_by name: params[:ad_name]
+    @event = SMSEvent.create(user_location: params[:user_location], user_phone_id: params[:user_phone_id], sender: params[:sender], receiver: params[:receiver], message: params[:message])
+    @advert.events << @event
+    @advert.save
   end
 
   def map
-    @event = MapEvent.new(params[:event])
-    @event.save
+    @advert = Advertisement.find_by name: params[:ad_name]
+    @event = MapEvent.create(user_location: params[:user_location], user_phone_id: params[:user_phone_id], map_location: params[:address])
+    @advert.events << @event
+    @advert.save
   end
 
   def call
-    @event = CallEvent.new(params[:event])
-    @event.save
+    @advert = Advertisement.find_by name: params[:ad_name]
+    @event = CallEvent.create(user_location: params[:user_location], user_phone_id: params[:user_phone_id], sender: params[:sender], receiver: params[:receiver])
+    @advert.events << @event
+    @advert.save
   end
 
   def index
